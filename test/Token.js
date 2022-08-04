8c6d514ab6c0da9036bbaff2cf58714379afb414
Juan Carlos - EscuelaCryptoES <escuelacryptodevs@gmail.com>
2022-08-04 14:35:02 +0200

    Unit testing script example

--- test/Token.js
+++ test/Token.js
@@ -5,21 +5,38 @@ const { ethers } = require("hardhat");
 require("@nomiclabs/hardhat-waffle");
 
 
-describe("crowdfunding", funtion () {
-    let crowdfunding: Contract;
-
-    beforeEach(async () => {
-        // Deploy the contract
+describe("Deploys the contract", function () {
+    this.beforeAll(async function(){
+      try{
+        provider = waffle.provider;
+  
+        //Market
         const Crowdfunding = await ethers.getContractFactory("Crowdfunding");
         crowdfunding = await Crowdfunding.deploy();
         await crowdfunding.deployed();
+      } 
+      catch(ex){
+        console.error(ex);
+      }
+    })
+    it("Deployed all contracts", async function () {
     });
+  });
 
-});
-
-it("Should start a project", async function () {
-    const test = await Contract.inicioProyecto(titulo,descripcion,_inicio,_final,cantidadARecaudar);
-    expect(test).to.equal(nuevoProyecto)
-
-});
+// Test suite
+describe("Should start a project", function(){
+    //Check if require works as intended
+    it("Fails if the start time is inferior to now", async function(){
+        await expect(crowdfunding.inicioProyecto("Test","Description",1,1,100)).to.be.revertedWith("inicio < now");
+    });
+    
+    //Check if success at a function
+    it("Starts a project", async function(){
+        let latestBlock = await provider.getBlock("latest");
+        let futureDate = latestBlock.timestamp+100;
+        await expect(crowdfunding.inicioProyecto("Test","Description",futureDate, futureDate+200, 100), "Starts the project").to.emit(crowdfunding, "proyectoIniciado");
+        let listaProyectos = await crowdfunding.listaTodosProyectos();
+        await expect(listaProyectos.length).to.equal(1);
+    });
+  });
 
